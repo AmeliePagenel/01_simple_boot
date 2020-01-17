30a9f27277c38590a6f6c436605fd4a0dfbeb194
mm <mm@gmail.com>
2020-01-17 20:22:36 +0100

    Add Spring Boot skeleton

---
+++ src/test/java/com/formation/_simple_boot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formation._simple_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
